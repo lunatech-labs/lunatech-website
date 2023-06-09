083afc5e4dc5a86357a63ea9157463580e9e6726
gbouet <geoffroy.bouet@lunatech.fr>
2023-06-09 17:03:07 +0200

    apply theme & responsive

--- public/javascripts/theme.js
+++ public/javascripts/theme.js
@@ -1,13 +1,13 @@
 console.clear()
 
-const themes = ['light', 'dark', 'neon'];
+const themes = ['light', 'dark'];
 let count = 0;
 const themePicker = document.getElementById('themePicker');
 const themeList = document.getElementById('themeGrid');
 
 // Change the CSS variables on the root element, depending on the curent theme
 const changeTheme = (theme) => {
-	if(count < 2) {
+	if(count < 1) {
 		count += 1;
 	} else {
 		count = 0
@@ -16,6 +16,7 @@ const changeTheme = (theme) => {
     document.documentElement.style.setProperty('--surface', `var(--${theme}-surface)`);
 	document.documentElement.style.setProperty('--bg', `var(--${theme}-bg)`);
     document.documentElement.style.setProperty('--bottom-bg', `var(--${theme}-bottom-bg)`);
+	document.documentElement.style.setProperty('--draw-bg', `var(--${theme}-draw-bg)`);
     document.documentElement.style.setProperty('--border', `var(--${theme}-border)`);
     document.documentElement.style.setProperty('--primary', `var(--${theme}-primary)`);
 	document.documentElement.style.setProperty('--text-primary', `var(--${theme}-text-primary)`);
@@ -35,13 +36,10 @@ const changeTheme = (theme) => {
 	
 	switch(theme) {
         case theme = 'light':
-			themeGrid.style.top = '13px'
+			themeGrid.style.top = '9px'
 			break;
 		case theme = 'dark':
-			themeGrid.style.top = '-23px'
-			break;
-		case theme = 'neon':
-			themeGrid.style.top = '-59px'
+			themeGrid.style.top = '-27px'
 			break;
 	}
 }
@@ -49,15 +47,12 @@ const changeTheme = (theme) => {
 // Define Icons
 const lightIcon = `<svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" tabindex="-1" title="Light"><path d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6zm0-10c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"></path></svg>`
 const darkIcon = `<svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" tabindex="-1" title="Dark"><path d="M10 2c-1.82 0-3.53.5-5 1.35C7.99 5.08 10 8.3 10 12s-2.01 6.92-5 8.65C6.47 21.5 8.18 22 10 22c5.52 0 10-4.48 10-10S15.52 2 10 2z"></path></svg>`
-const neonIcon = `<svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" tabindex="-1" title="Sunrise"><path d="M20 15.31 23.31 12 20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69zM12 18V6c3.31 0 6 2.69 6 6s-2.69 6-6 6z"></path></svg>`
-
 
 // Content and click function for the theme picker
 themePicker.innerHTML = `
 	<div class="c-theme__grid">
         ${lightIcon}
 		${darkIcon}
-		${neonIcon}
 	</div>
 `
 
