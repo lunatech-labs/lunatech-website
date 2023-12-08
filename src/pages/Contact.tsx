d1b3c8931d9052957443580147947ffd375700da
Geoffroy Bouet <39944843+geoffroybouet@users.noreply.github.com>
2023-12-08 14:44:07 +0100

    adjust parallax structure

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import Stars from "../components/Stars/Stars";
 
 const Contact = () => {
     return (
-        <div>
-            <p>Je suis une page contact</p>
+        <div className="App">
+            <Parallax pages={2} style={{ top: '0', left: '0' }} className="animation">
+                <ParallaxLayer offset={0} speed={0.25}><div className="animation_layer parallax" id="artback"><Stars /></div></ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.3}><div className="animation_layer parallax" id="mist"></div></ParallaxLayer>
+                <ParallaxLayer offset={0} speed={-0.1}><div className="animation_layer parallax" id="logoland"></div></ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.3}><div className="animation_layer parallax" id="clouds"></div></ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.35}><div className="animation_layer parallax" id="jungle2"></div></ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.5}><div className="animation_layer parallax" id="jungle3"></div></ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.45}>
+                    <div className="animation_layer parallax" id="jungle4"></div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.40}>
+                    <div className="animation_layer parallax" id="manonmountain"></div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.35}>
+                    <div className="animation_layer parallax" id="jungle5"></div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={1} speed={0.25}>
+                    <div id="textblock">
+                        <div id="textblock-container">
+                            <h1 id="textblock-title">What is Firewatch?</h1>
+                            <p id="textblock-content">
+                            The year is 1989.<br/><br/>
+                            You are a man named Henry who has retreated from your messy life to work as a fire lookout in the Wyoming wilderness. Perched atop a mountain, it's your job to find smoke and keep the wilderness safe.<br/><br/>
+                            An especially hot, dry summer has everyone on edge. Your supervisor, a woman named Delilah, is available to youat all times over a small, handheld radio—and is your only contact with the world you've left behind.<br/><br/>
+                            But when something strange draws you out of your lookout tower and into the world below, you'll explore a wild and unknown environment, facing questions and making interpersonal choices that can build or destroy the only meaningful relationship you have.
+                            </p>
+                        </div>
+                        <footer id='textblock-footer'>Demo Created With 🧡 By&nbsp;<a id="textblock-devsense" href="https://youtube.com/c/DevSense19">DevSense</a></footer>
+                    </div>
+                </ParallaxLayer>
+            </Parallax>
         </div>
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,33 +5,33 @@ const Home = () => {
 
     return (
         <>
-            <div className="hero">
-                <Parallax pages={4}>
-                    <ParallaxLayer offset={0} speed={.01}>
+            <Parallax pages={2}>
+                <ParallaxLayer offset={0} speed={0.25}>
+                    <div className="hero">
                         <Stars />
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={.09}>
-                        <div className="landscape__clouds"></div>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={.2}>
-                        <div className="landscape__mountains"></div>
-                    </ParallaxLayer>
-                    <ParallaxLayer offset={0} speed={.4}>
-                        <div className="landscape__grass"></div>
-                    </ParallaxLayer>
-                </Parallax>
-                <div className="layer__header">
-                    <h1>Modernize your <span>digital products</span> for tomorrow's future.</h1>
-                    <p>We think and design unique experiences for tomorrow's innovative digital products.</p>
-                </div>
-            </div>
-
-            <article className="main-article">
-                <div className="main-article__content">
-                    <h2 className="main-article__header">To be continued</h2>
-                    <p className="main-article__paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis error provident dignissimos facere. Repellendus tempore autem qui! Quia magnam tempora esse id necessitatibus corrupti mollitia expedita sapiente cum rerum, ut dicta laboriosam!</p>
-                </div>
-            </article>
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.30}><div className="landscape__mist"></div></ParallaxLayer>
+                <ParallaxLayer offset={0} speed={0.2}><div className="landscape__clouds"></div></ParallaxLayer>
+                <ParallaxLayer offset={0} speed={-0.5}>
+                    <div className="hero-content">
+                        <h1>Modernize your<br /><span>digital products</span><br />for tomorrow's future.</h1>
+                        <p>We think and design unique experiences<br />for tomorrow's innovative digital products.</p>
+                        <a href="#">Let's talk</a>
+                    </div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={.2}><div className="landscape__mountains"></div></ParallaxLayer>
+                <ParallaxLayer offset={0} speed={.4}><div className="landscape__grass"></div></ParallaxLayer>
+                <ParallaxLayer offset={0} speed={.4}><div className="landscape__telescope"></div></ParallaxLayer>
+                <ParallaxLayer offset={1} speed={.2}>
+                    <article className="main-article">
+                        <div className="main-article__content">
+                            <h2 className="main-article__header">To be continued</h2>
+                            <p className="main-article__paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis error provident dignissimos facere. Repellendus tempore autem qui! Quia magnam tempora esse id necessitatibus corrupti mollitia expedita sapiente cum rerum, ut dicta laboriosam!</p>
+                        </div>
+                    </article>
+                </ParallaxLayer>
+            </Parallax>
         </>
     );
 };
