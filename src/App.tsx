cf2704b12171f69322ab8df5f1ebf398fd79a1f9
Geoffroy Bouet <39944843+geoffroybouet@users.noreply.github.com>
2024-02-06 18:46:53 +0100

    add-sticky-header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
+import useScrollListener from "./components/Header/useScrollListener";
+
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import RoutesMap from './RoutesMap';
 import "./App.scss";
-import "./index.css";
 
 function App() {
+
+    const [navClassList, setNavClassList] = useState([]);
+    const scroll = useScrollListener();
+
+    // update classList of nav on scroll
+    useEffect(() => {
+        const _classList = [];
+
+        if (scroll.y > 150 && scroll.y - scroll.lastY > 0)
+            _classList.push("nav-bar--hidden");
+        
+            setNavClassList(_classList);
+    }, [scroll.y, scroll.lastY]);
+
     return (
         <>
-            <Header />
+            <nav className={navClassList.join(" ")}>
+                <Header />
+            </nav>
             <RoutesMap />
             <Footer />
         </>
