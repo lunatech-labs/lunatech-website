9b965c9a365b7665a5fade36e7b90be3c8621e3c
Lucas Leblanc <44496264+Dedelweiss@users.noreply.github.com>
2024-02-13 16:05:38 +0100

    Refactor: Improve button in design page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,6 +4,7 @@ import Input from '@components/Input/Input';
 import Address from '@components/Adress/Adress';
 import Container from '@components/Container/Container';
 import Grid from '@components/Grid/Grid';
+import ArrowSend from '/arrow-send.svg';
 import ButtonPrimary from '@components/ButtonPrimary/ButtonPrimary';
 
 const Contact = () => {
@@ -30,18 +31,20 @@ const Contact = () => {
                             <form>
                                 <Input number='01' type='text' label='What&apos;s your name?' placeHolder='Type your full name' name='fullName'>
                                 </Input>
-                                <Input number='02' type='email' label='What&apos;s your email address?' placeHolder='example@email.com' name='fullName'>
+                                <Input number='02' type='email' label='What&apos;s your email address?' placeHolder='example@email.com' name='email'>
                                 </Input>
-                                <Input number='03' type='text' label='What&apos;s your phone number?' placeHolder='+11 2222 333344' name='fullName'>
+                                <Input number='03' type='text' label='What&apos;s your phone number?' placeHolder='+11 2222 333344' name='phoneNumber'>
                                 </Input>
-                                <Input number='04' type='text' label='What&apos;s your company/organization name?' placeHolder='Type your company/organisation name' name='fullName'>
+                                <Input number='04' type='text' label='What&apos;s your company/organization name?' placeHolder='Type your company/organisation name' name='company'>
                                 </Input>
-                                <Input number='05' type='options' options={options} label='What&apos;s services are you looking for?' placeHolder='Choose from the list here' name='fullName'>
+                                <Input number='05' type='options' options={options} label='What&apos;s services are you looking for?' placeHolder='Choose from the list here' name='services'>
                                 </Input>
-                                <Input number='06' type='textarea' label='Tell us about your project' placeHolder='Please type your project description' name='fullName'>
+                                <Input number='06' type='textarea' label='Tell us about your project' placeHolder='Please type your project description' name='projectDescription'>
                                 </Input>
                             </form>
-                            <button className="formBtn" type="submit" form="form1" value="Submit">Send message</button>
+                            <ButtonPrimary iconUrl={ArrowSend} size="large" to="/">
+                                <span>Send message</span>
+                            </ButtonPrimary>
                         </div>
                         <div className="contact__infos">
                             <Address country='Netherlands' kind='Business development' mail='info@lunatech.nl' phone='+31 10 750 2600' street='Hofplein 20' city='3032 AC Rotterdam'/>
