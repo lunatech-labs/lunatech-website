0e8360db225bd15ea8179a0ef4075895df2553dd
Lucas Leblanc <44496264+Dedelweiss@users.noreply.github.com>
2024-02-16 16:24:24 +0100

    refactor classnames and scss

--- src/components/OfficeModal/OfficeModal.tsx
+++ src/components/OfficeModal/OfficeModal.tsx
@@ -27,55 +27,57 @@ const OfficeModal = (props: ModalProps) => {
     const { t, i18n } = useTranslation();
     
     return (
-    <div className='modal'>
+    <div className='office-modal'>
         <div onClick={props.handleClose} className="overlay"></div>
-        <div className='modal-content'>
-            <div className='modal-content__button'>
+        <div className='office-modal__content'>
+            <div className='office-modal__close-btn'>
                 <button type="button" onClick={props.handleClose}><img src={CloseIcon} className="" alt="Add" /></button>
             </div>
-            <div className='modal-content__body'>
-                <div className='modal-content__title'>
+            <div className='office-modal__body'>
+                <div className='office-modal__header'>
                     {props.children}
                     <div>
                         <Category content="Our offices"/>
                         <Title content={<><span>{props.title}</span> office</>} />
                     </div>
                 </div>
-                <div className="modal-content__img">
+                <div className="office-modal__image">
                     <img className="officeImg" src={NewCastleOffice} alt="" />
                 </div>
                 <Text className="mgb64">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sit, vero veniam consequatur ad perferendis ipsam debitis! Quia quas, mollitia dolores voluptatem voluptates vel voluptatibus fugiat? Modi minus illo explicabo!</Text>
-                <div className='modal-content__infos'>
-                    {(props.email && props.phone) && (
-                        <div>
-                            <Text>Business development</Text>
-                            <p>
-                                <img src={EmailIcon} alt="" />{props.email}
-                                <span>|</span>
-                                <img src={PhoneIcon} alt="" />{props.phone}
-                            </p>
-                        </div>
-                    )}
-                    {(props.careersEmail) && (
-                        <div>
-                            <Text>Careers</Text>
-                            <p><img src={EmailIcon} alt="" />{props.careersEmail}</p>
-                        </div>  
-                    )}
-                        <div className='modal-content__address'>
-                            {(props.address && props.country) && (
-                                <div>
-                                    <Text>Address</Text>
-                                    <p><img src={HomeIcon} alt="" />{props.address}</p>
-                                    <p><img src={FlagIcon} alt="" />{props.country}</p>
-                                </div>
-                            )}
-                            <ButtonPrimary iconUrl={Message} size='large' to='/contact'>
+                <div className='office-modal__info'>
+                    <div className="office-modal__contact-details">
+                        {(props.email && props.phone) && (
+                            <div className="office-modal__business-contact">
+                                <Text>Business development</Text>
+                                <p>
+                                    <img src={EmailIcon} alt="" />{props.email}
+                                    <span>|</span>
+                                    <img src={PhoneIcon} alt="" />{props.phone}
+                                </p>
+                            </div>
+                        )}
+                        {(props.careersEmail) && (
+                            <div className="office-modal__careers-contact">
+                                <Text>Careers</Text>
+                                <p><img src={EmailIcon} alt="" />{props.careersEmail}</p>
+                            </div>  
+                        )}
+                        {(props.address && props.country) && (
+                            <div className='office-modal__address'>
+                                <Text>Address</Text>
+                                <p><img src={HomeIcon} alt="" />{props.address}</p>
+                                <p><img src={FlagIcon} alt="" />{props.country}</p>
+                            </div>
+                        )}
+                    </div>
+                    <div className="office-modal__contact-btn">
+                        <ButtonPrimary iconUrl={Message} size='large' to='/contact'>
                                 <span>
                                     {t('modal.contactUsBtn')}
                                 </span>
-                            </ButtonPrimary>
-                        </div>
+                        </ButtonPrimary>
+                    </div>
                 </div>
             </div>
         </div>
