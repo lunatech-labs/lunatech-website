91e8cdf692efae787569e4a1c92bb0fea7e43fe4
Geoffroy Bouet <39944843+geoffroybouet@users.noreply.github.com>
2024-02-19 20:48:05 +0100

    footer translation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import { useTranslation, Trans } from 'react-i18next';
 import Container from "@components/Container/Container";
 import Grid from "@components/Grid/Grid";
 import Language from "@components/Language/Language";
@@ -9,6 +10,8 @@ import Twitter from '/twitter.svg';
 import "./Footer.scss";
 
 const Footer = () => {
+    useTranslation();
+
     return (
         <footer className="footer">
             <Container>
@@ -17,7 +20,7 @@ const Footer = () => {
                         <div className="footer__part">
                             <div className="footer__brand">
                                 <a href="/" target="_blank"><img src={lunatechLogo} className="footer__logo" alt="Lunatech logo" /></a>
-                                <p className="footer__text">Managed by Lunatech Labs.</p>
+                                <p className="footer__text"><Trans i18nKey="footer.managed" /></p>
                             </div>
                             <div className="footer__social">
                                 <a href="https://github.com/lunatech-labs" target="_blank"><img src={Github} className="footer__icon" alt="Github logo" /></a>
@@ -28,19 +31,19 @@ const Footer = () => {
                         </div>
                         <div className="footer__part">
                             <div className="footer__category">
-                                <p className="footer__title">Plan</p>
+                                <p className="footer__title"><Trans i18nKey="footer.title1" /></p>
                                 <ul className="footer__list">
-                                    <li className="footer__item"><a href="/">Home</a></li>
-                                    <li className="footer__item"><a href="/about">About Us</a></li>
+                                    <li className="footer__item"><a href="/"><Trans i18nKey="navigation.home" /></a></li>
+                                    <li className="footer__item"><a href="/about"><Trans i18nKey="navigation.about" /></a></li>
                                     <li className="footer__item"><a href="/services">Services</a></li>
-                                    <li className="footer__item"><a href="/works">Works</a></li>
+                                    <li className="footer__item"><a href="/vision">Vision</a></li>
                                 </ul>
                             </div>
                         </div>
 
                         <div className="footer__part">
                             <div className="footer__category">
-                                <p className="footer__title">Get Connected</p>
+                                <p className="footer__title"><Trans i18nKey="footer.title2" /></p>
                                 <ul className="footer__list">
                                     <li className="footer__item"><a href="https://blog.lunatech.com/" target="_blank">Blog</a></li>
                                     <li className="footer__item"><a href="https://recruitment.lunatech.com/" target="_blank">Careers</a></li>
@@ -56,8 +59,8 @@ const Footer = () => {
                 </div>
                 
                 <div className="footer__bottom">
-                    <p>© 2023 Lunatech. All rights reserved.</p>
-                    <a href="/Lunatech-General_Terms_and_Conditions.pdf" target="_blank">Terms of Services</a>
+                    <p>© 2023 Lunatech. <Trans i18nKey="footer.copyright" /></p>
+                    <a href="/Lunatech-General_Terms_and_Conditions.pdf" target="_blank"><Trans i18nKey="footer.terms" /></a>
                 </div>
             </Container>
         </footer>
