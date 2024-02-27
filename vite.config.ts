4a29e1f02551fad8da931d49e883c265f8baadb6
Lucas Leblanc <44496264+Dedelweiss@users.noreply.github.com>
2024-02-27 16:52:50 +0100

    Fix: deploy clever cloud (#156)

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   server: {
     host: '0.0.0.0',
     port: 8080,
