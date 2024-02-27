771bb09c23a53f8820415b35f7267e1c7cab019e
Geoffroy Bouet <39944843+geoffroybouet@users.noreply.github.com>
2024-02-27 17:06:12 +0100

    Services details page: Create each page for each services

---
+++ src/pages/Services/ServiceLegacyTransformation.tsx
@@ -0,0 +1,50 @@
+import { Trans } from 'react-i18next';
+import Section from "@components/Section/Section";
+import Container from "@components/Container/Container";
+import Grid from "@components/Grid/Grid";
+import Breadcrumbs from "@components/Breacrumb/Breadcrumb";
+import BorderEffect from '@components/BorderEffect/BorderEffect';
+import Category from '@components/Category/Category';
+import Title from '@components/Title/Title';
+import ButtonPrimary from '@components/ButtonPrimary/ButtonPrimary';
+import Address from '@components/Address/Address';
+
+import Message from '/message.svg';
+
+const ServiceLegacyTransformation = () => {
+    return (
+        <>
+            <Section className="pdt256">
+                <Container>
+                    <Grid>
+                        <div className="col-lg-6">
+                            <Breadcrumbs />
+                            <h1 className="hero__title"><Trans i18nKey="serviceLegacyTransformation.title" /></h1>
+                        </div>
+                    </Grid>
+                </Container>
+            </Section>
+
+            <Container><BorderEffect /></Container>
+
+            <Section>
+                <Container>
+                    <div className="contact">
+                        <div>
+                            <Category content={<Trans i18nKey="contact.subtitle" />} />
+                            <Title content={<>Let's talk <span>together!</span></>} />
+                        </div>
+                        <ButtonPrimary iconUrl={Message} size='large' to='/contact'>
+                            <span>
+                                Contact Us
+                            </span>
+                        </ButtonPrimary>
+                    </div>
+                    <Address />
+                </Container>
+            </Section>
+        </>
+    )
+}
+
+export default ServiceLegacyTransformation;
