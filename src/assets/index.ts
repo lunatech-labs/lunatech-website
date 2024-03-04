4451ade0c06cd31a6601818d17348e6cabb43701
Lucas Leblanc <44496264+Dedelweiss@users.noreply.github.com>
2024-03-04 17:15:47 +0100

    Feat constant file (#195)
    * Feat: add vertical timeline block
    
    * Refactor: put constants in a specific file

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,10 +2,24 @@ import instagram from "./instagram.svg";
 import twitter from "./twitter.svg";
 import linkedin from "./linkedin.svg";
 import github from "./github.svg";
+import netherlandsIcon from "./netherlandsIcon.svg";
+import franceIcon from "./franceIcon.svg";
+import ukIcon from "./ukIcon.svg";
+import sdvIcon from "./cpu-setting.svg";
+import customSoftwareIcon from "./custom-software.svg";
+import systemIntegrationIcon from "./system-integration.svg";
+import legacyTransformationIcon from "./legacy-transformation.svg";
 
 export {
     instagram,
     twitter,
     linkedin,
-    github
+    github,
+    netherlandsIcon,
+    franceIcon,
+    ukIcon,
+    sdvIcon,
+    customSoftwareIcon,
+    systemIntegrationIcon,
+    legacyTransformationIcon
 }
