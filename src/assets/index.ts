ad4864f3ebfcb210ea431320eb4db6081f483188
Lucas Leblanc <44496264+Dedelweiss@users.noreply.github.com>
2024-03-04 13:52:42 +0100

    Feat: add vertical timeline block (#194)

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import instagram from "./instagram.svg";
+import twitter from "./twitter.svg";
+import linkedin from "./linkedin.svg";
+import github from "./github.svg";
+
+export {
+    instagram,
+    twitter,
+    linkedin,
+    github
+}
