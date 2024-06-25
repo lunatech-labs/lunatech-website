d71c908fa34a441ed7599f2770eb79ec2c6c9f15
Lucas Leblanc <44496264+Dedelweiss@users.noreply.github.com>
2024-06-25 10:02:26 +0200

    corrections

--- src/components/TechnologiesCarousel/TechnologiesCarousel.tsx
+++ src/components/TechnologiesCarousel/TechnologiesCarousel.tsx
@@ -10,6 +10,7 @@ import ButtonSecondary from '@components/ButtonSecondary/ButtonSecondary';
 import "./TechnologiesCarousel.scss";
 import { arrowChevronLeft, arrowChevronRight, arrowRight } from '@/assets';
 import { technologiesPage } from '@/constants';
+import IconAnimated from '../IconAnimated/IconAnimated';
 
 const TechnologiesCarousel = () => {
     const wrapperRef = useRef(null);
@@ -51,15 +52,6 @@ const TechnologiesCarousel = () => {
                 <ButtonSecondary iconUrl={arrowRight} size='large' to='/technologies'>{<Trans i18nKey="buttonDetails" />}</ButtonSecondary>
             </div>
             <div className='technologies-carousel__content'>
-                <div className="technologies-carousel__slider" ref={wrapperRef}>
-                    <Spotlight className="spotlight__layout">
-                        {technologiesPage[0].secondBlock.map((technology,index) => 
-                            <SpotlightCard key={index} padding="pad48">
-                                <TechnologieCardCarousel title={technology.title} text={technology.text}><Icon imageUrl={technology.imageUrl} size="medium" /></TechnologieCardCarousel>
-                            </SpotlightCard>
-                        )}
-                    </Spotlight>
-                </div>
                 <div className='technologies-carousel__buttons'>
                     <button className={`${isAtStart ? 'disabled' : ''}`} onClick={scrollLeft}><img src={arrowChevronLeft} alt="Scroll Left" /></button>
                     <button className={`${isAtEnd ? 'disabled' : ''}`} onClick={scrollRight}><img src={arrowChevronRight} alt="Scroll Right" /></button>
