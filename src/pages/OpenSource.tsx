c48a4024458b72b59ac22ebd1e313c5252f3ff8b
Nicolas Leroux <nicolas@lunatech.com>
2024-08-26 12:02:44 +0200

    attempted to format the open source page

--- src/pages/OpenSource.tsx
+++ src/pages/OpenSource.tsx
@@ -15,6 +15,7 @@ import { aboutUsIllustration } from '@/assets';
 const OpenSource = () => {
     return (
         <>
+
             <Section className="pdt256">
                 <Container>
                     <HeaderPage>
@@ -24,13 +25,119 @@ const OpenSource = () => {
                 </Container>
             </Section>
 
+            <Section>
+                <Container>
+                    <div className="opensource">
+                        <img className="opensource__img" src={aboutUsIllustration} alt="" />
+                        <div className="opensource__content">
+                            <Text size="large" className="mgb32">
+                                <Trans i18nKey="opensource.text1" />
+                            </Text>
+                        </div>
+                        <div className="opensource__content2">
+                            <Text size="large" className="mgb32">
+                                <ol>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list1.title" /></em>
+                                            <Trans i18nKey="opensource.list1.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list2.title" /></em>
+                                            <Trans i18nKey="opensource.list2.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list3.title" /></em>
+                                            <Trans i18nKey="opensource.list3.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list4.title" /></em>
+                                            <Trans i18nKey="opensource.list4.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list5.title" /></em>
+                                            <Trans i18nKey="opensource.list5.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list6.title" /></em>
+                                            <Trans i18nKey="opensource.list6.text" />
+                                        </div>
+                                    </li>
+                                </ol>
+                                
+                                <Trans i18nKey="opensource.text2" />
+                            </Text>
+                        </div>
+                    </div>
+                </Container>
+            </Section>
+
+            <Container><BorderEffect /></Container>
+
             <Section className="bg-bgMediumBlue">
                 <Container>
-                        <img className="opensource__img" src={aboutUsIllustration} alt="Building" />
-                        <div >
-                            <Text size="medium" className="mgb24 opensource__text"><Trans i18nKey="opensource.text" /></Text>
+                    <div className="opensource__partners">
+                        <div className="opensource__partnersText">
+                            <Text size="large" className="mgb32">
+                                <Trans i18nKey="opensource.text3" />
+                                <ol>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list7.title" /></em>
+                                            <Trans i18nKey="opensource.list7.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list8.title" /></em>
+                                            <Trans i18nKey="opensource.list8.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list9.title" /></em>
+                                            <Trans i18nKey="opensource.list9.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list10.title" /></em>
+                                            <Trans i18nKey="opensource.list10.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list11.title" /></em>
+                                            <Trans i18nKey="opensource.list11.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list12.title" /></em>
+                                            <Trans i18nKey="opensource.list12.text" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <em><Trans i18nKey="opensource.list13.title" /></em>
+                                            <Trans i18nKey="opensource.list13.text" />
+                                        </div>
+                                    </li>
+                                </ol>
+                                <Trans i18nKey="opensource.text4" />
+                            </Text>
                         </div>
-                       
+                    </div>
                 </Container>
             </Section>
 
