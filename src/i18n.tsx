789eee7d2933b69e9b652e565c7030c92455cdbd
Nicolas Leroux <nicolas@lunatech.com>
2024-08-27 08:25:38 +0200

    add custom resolver for langage

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -3,13 +3,25 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+import customPath from './customLanguageDetector';
+
+const languageDetector = new LanguageDetector();
+
+languageDetector.addDetector(customPath);
+
+const options = {
+    order: ['customPath', 'localStorage', 'querystring',  'navigator'],
+    // reset of options
+};
+
+
 i18n
   // load translation using http -> see /public/locales
   // learn more: https://github.com/i18next/i18next-http-backend
   .use(Backend)
   // detect user language
+  .use(languageDetector)
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
@@ -17,6 +29,7 @@ i18n
   .init({
     fallbackLng: 'en',
     debug: false,
+    detection: options,
     interpolation: {
       escapeValue: false
     }
