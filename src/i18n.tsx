f81deb663dff91304c2b77b8c5f4fe6fd2233bfa
Nicolas Leroux <nicolas@lunatech.com>
2024-08-27 09:15:47 +0200

    add custom resolver for langage

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -18,16 +18,8 @@ languageDetector.addDetector({
         // In the browser, get the hostname from window.location.
         if (typeof window !== 'undefined' ) {
           locale = domainLocaleMap[window.location.hostname];
-        } 
-        // On the server, get the hostname from the request headers.
-        // We use the host header which is available on IncomingMessage.
-        // https://nodejs.org/api/http.html#http_class_http_incomingmessage
-        // But the host header may include the port so first we take that off, if it exists.
-        else {
-          const hostname = req.headers.host?.split(':')[0];
-          locale = domainLocaleMap[hostname];
-        }
-        return locale;
+        }      
+      return locale;
     }
 });
 
