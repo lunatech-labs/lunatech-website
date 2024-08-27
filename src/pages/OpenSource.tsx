3524fd78bddad1dea7ba90dc5a7567e7fb789bf3
Nicolas Leroux <nicolas@lunatech.com>
2024-08-27 15:27:08 +0200

    open source page layout

--- src/pages/OpenSource.tsx
+++ src/pages/OpenSource.tsx
@@ -9,7 +9,7 @@ import Address from '@components/Address/Address';
 import HeaderPage from '@components/HeaderPage/HeaderPage';
 import ContactBlock from '@components/ContactBlock/ContactBlock';
 import Text from '@components/Text/Text';
-
+import Grid from '@components/Grid/Grid';
 import { aboutUsIllustration } from '@/assets';
 
 const OpenSource = () => {
@@ -34,52 +34,47 @@ const OpenSource = () => {
                                 <Trans i18nKey="opensource.text1" />
                             </Text>
                         </div>
-                        <div className="opensource__content2">
-                            <Text size="medium" className="mgt24">
-                                <ol>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list1.title")}</Title>
-                                            <Trans i18nKey="opensource.list1.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list2.title")}</Title>
-                                            <Trans i18nKey="opensource.list2.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list3.title")}</Title>
-                                            <Trans i18nKey="opensource.list3.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                        <Title level={3}>{t("opensource.list4.title")}</Title>
-                                            <Trans i18nKey="opensource.list4.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list5.title")}</Title>
-                                            <Trans i18nKey="opensource.list5.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list6.title")}</Title>
-                                            <Trans i18nKey="opensource.list6.text" />
-                                        </div>
-                                    </li>
-                                    <Trans i18nKey="opensource.text2" />
-                                </ol>
-                                
-                            </Text>
                         </div>
-                    </div>
+                        <Grid className='opensource'>
+                            <div className="col-lg-6 col-md-12 spotlight__card ">
+                                <Title level={3} className="title" >{t("opensource.list1.title")}</Title>
+                                <Trans i18nKey="opensource.list1.text" />
+                            </div>
+                            <div className="col-lg-6 col-md-12 spotlight__card ">
+                                <Title level={3} className="title" >{t("opensource.list2.title")}</Title>
+                                <Trans i18nKey="opensource.list2.text" />
+                            </div>
+                            <div className="col-lg-6 col-md-12 spotlight__card ">
+                                <Title level={3} className="title" >{t("opensource.list3.title")}</Title>
+                                <Trans i18nKey="opensource.list4.text" />
+                            </div>
+                            <div className="col-lg-6 col-md-12 spotlight__card ">
+                                <Title level={3} className="title" >{t("opensource.list4.title")}</Title>
+                                <Trans i18nKey="opensource.list4.text" />
+                            </div>
+                            <div className="col-lg-6 col-md-12 spotlight__card ">
+                                <Title level={3} className="title" >{t("opensource.list5.title")}</Title>
+                                <Trans i18nKey="opensource.list5.text" />
+                            </div>
+                            <div className="col-lg-6 col-md-12 spotlight__card ">
+                                <Title level={3} className="title" >{t("opensource.list6.title")}</Title>
+                                <Trans i18nKey="opensource.list6.text" />
+                            </div>
+
+                        </Grid>
+                        <Text size="large" className="mgt32">
+                            <Trans i18nKey="opensource.text2" />
+                        </Text>
+                   
                 </Container>
+            </Section>
+
+            <Container><BorderEffect /></Container>
+
+            <Section className="bg-bgMediumBlue">
+                <Container>
+                        <Title level={1}><Trans i18nKey="opensource.text3" /></Title>
+                  </Container>
             </Section>
 
             <Container><BorderEffect /></Container>
@@ -88,58 +83,37 @@ const OpenSource = () => {
                 <Container>
                     <div className="opensource__partners">
                         <div className="opensource__partnersText">
-                            <Text size="large" className="mgt24">
-                                <Trans i18nKey="opensource.text3" /></Text>
-                            <Text size="medium" className="mgt24">
-                                <ol>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list7.title")}</Title>
-                                            <Trans i18nKey="opensource.list7.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list8.title")}</Title>
-                                            <Trans i18nKey="opensource.list8.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list9.title")}</Title>
-                                            <Trans i18nKey="opensource.list9.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list10.title")}</Title>
-                                            <Trans i18nKey="opensource.list10.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list11.title")}</Title>
-                                            <Trans i18nKey="opensource.list11.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list12.title")}</Title>
-                                            <Trans i18nKey="opensource.list12.text" />
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div>
-                                            <Title level={3}>{t("opensource.list13.title")}</Title>
-                                            <Trans i18nKey="opensource.list13.text" />
-                                        </div>
-                                        
-                                    </li>
-                                    <Trans i18nKey="opensource.text4" />
-                                </ol>
-                                
-                            </Text>
-                        </div>
+                            <Grid className='opensource'>
+                                <div className="col-lg-6 col-md-12 spotlight__card ">
+                                    <Title level={3} className="title" >{t("opensource.list7.title")}</Title>
+                                    <Trans i18nKey="opensource.list7.text" />
+                                </div>
+                                <div className="col-lg-6 col-md-12 spotlight__card ">
+                                    <Title level={3} className="title" >{t("opensource.list8.title")}</Title>
+                                    <Trans i18nKey="opensource.list8.text" />
+                                </div>
+                                <div className="col-lg-6 col-md-12 spotlight__card ">
+                                    <Title level={3} className="title" >{t("opensource.list9.title")}</Title>
+                                    <Trans i18nKey="opensource.list9.text" />
+                                </div>
+                                <div className="col-lg-6 col-md-12 spotlight__card ">
+                                    <Title level={3} className="title" >{t("opensource.list10.title")}</Title>
+                                    <Trans i18nKey="opensource.list10.text" />
+                                </div>
+                                <div className="col-lg-6 col-md-12 spotlight__card ">
+                                    <Title level={3} className="title" >{t("opensource.list11.title")}</Title>
+                                    <Trans i18nKey="opensource.list11.text" />
+                                </div>
+                                <div className="col-lg-6 col-md-12 spotlight__card ">
+                                    <Title level={3} className="title" >{t("opensource.list12.title")}</Title>
+                                    <Trans i18nKey="opensource.list12.text" />
+                                </div>
+                                <div className="col-lg-6 col-md-12 spotlight__card ">
+                                    <Title level={3} className="title" >{t("opensource.list1.title")}</Title>
+                                    <Trans i18nKey="opensource.list13.text" />
+                                </div>
+                            </Grid>
+                       </div>
                     </div>
                 </Container>
             </Section>
