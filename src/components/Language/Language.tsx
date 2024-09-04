808bc98ad5d57b3b1f5cfa53c72e2182dab49eb9
Nicolas Leroux <nicolas@lunatech.com>
2024-09-04 21:11:47 +0200

    removed unused import

--- src/components/Language/Language.tsx
+++ src/components/Language/Language.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import i18n from '../../i18n';
 //import { useTranslation } from 'react-i18next';
-import { redirect, redirectDocument, replace } from 'react-router-dom';
 
 import "./Language.scss";
 
